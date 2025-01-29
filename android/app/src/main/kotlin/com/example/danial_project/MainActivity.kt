b2896d3ea2b4afd0fc61c00b271b127ba148e2e3
Muhammad Ali Nizami <kustalics@gmail.com>
2025-01-29 12:29:40 +0500

    First Commit

---
+++ android/app/src/main/kotlin/com/example/danial_project/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.danial_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/bottom_bar.dart
@@ -0,0 +1,40 @@
+import 'package:danial_project/favorite.dart';
+import 'package:danial_project/home.dart';
+import 'package:danial_project/profile.dart';
+import 'package:flutter/material.dart';
+
+class BottomBarDemo extends StatefulWidget {
+  const BottomBarDemo({super.key});
+
+  @override
+  State<BottomBarDemo> createState() => _BottomBarDemoState();
+}
+
+class _BottomBarDemoState extends State<BottomBarDemo> {
+  int selectedIndex = 0;
+
+  List<Widget> screenList = [
+    HomeView(),
+    FavoriteView(),
+    ProfileView(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: screenList.elementAt(selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+          onTap: (val) {
+            selectedIndex = val;
+            setState(() {});
+          },
+          currentIndex: selectedIndex,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.favorite), label: 'Favorite'),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+          ]),
+    );
+  }
+}
